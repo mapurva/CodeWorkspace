d48bc65e5d9f1dcdb41d72a19d5727e0f8e3cdeb
Apurva Mehta <apurvamehta2015@gmail.com>
2023-01-16 15:19:53 +0530

    comment added

--- C/CtoF.c
+++ C/CtoF.c
@@ -4,7 +4,7 @@ int main()
     float c; // centigrade
     float f; // fahrenheit
     c = 40;
-    f = ((9 * c) / 5) + 32;
+    f = ((9 * c) / 5) + 32; // C to F convert
 
     printf("The temperature %f Celsius ", c);
     printf("quals %f Fahrenheit.", f);
