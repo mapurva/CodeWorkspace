53641fe5143a75ed26cc87604638108860555022
Apurva Mehta <apurvamehta2015@gmail.com>
2023-01-16 15:54:55 +0530

    initial commit

--- C/CheckLeapYear.c
+++ C/CheckLeapYear.c
@@ -5,6 +5,7 @@ int main()
     int year = 0;
     printf("Enter a year\n");
     scanf("%d", &year);
+    // every 4th year is a leap year, but not every 100th year unless it is a 400th year
     if ((year % 4 == 0) && (((!(year % 100 == 0))) || (year % 400 == 0)))
     {
         printf("The year %d is a leap year", year);
