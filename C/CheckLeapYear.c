a7d3d8a4be815840d1654a9b13012f7b8864c087
Apurva Mehta <apurvamehta2015@gmail.com>
2023-01-16 15:51:51 +0530

    intial commit

---
+++ C/CheckLeapYear.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int year = 0;
+    printf("Enter a year\n");
+    scanf("%d", &year);
+    if ((year % 4 == 0) && (((!(year % 100 == 0))) || (year % 400 == 0)))
+    {
+        printf("The year %d is a leap year", year);
+    }
+    else
+    {
+        printf("The year %d is not a leap year", year);
+    }
+    return 0;
+}
