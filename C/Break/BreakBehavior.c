bfad13392d658961d379da83485cc576698169af
Apurva Mehta <apurvamehta2015@gmail.com>
2023-01-28 11:09:52 +0530

    initial commit

---
+++ C/Break/BreakBehavior.c
@@ -0,0 +1,14 @@
+// Break exits the loop immediately
+// does not go to update expression
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        if (i % 2 == 0)
+            break;
+    }
+    printf("%d\n", i);
+    return 0;
+}
