fed75ac75f628f0eb361be9105d400296b14a19a
Apurva Mehta <apurvamehta2015@gmail.com>
2023-03-07 14:31:52 +0530

    add more print statements

--- C/Pointers/basics.c
+++ C/Pointers/basics.c
@@ -1,13 +1,32 @@
 // Introduction to pointers in C/C++
 // https://www.youtube.com/@mycodeschool/playlists
+// file1
 #include <stdio.h>
 int main()
 {
     int a;
     a = 5;
     int *p;
+    // printf("%d \n", p);
+    // printf("%d \n", *p);
     p = &a;
     printf("%d \n", a);
+    printf("%d \n", &a);
     printf("%d \n", p);
+    printf("%d \n", &p);
+    printf("%d \n", *p);
+    *p = 9;
+    printf("%d \n", a);
+    int b = 10;
+    *p = b;
+    printf("%d \n", p);
+    printf("%d \n", &p);
+    printf("%d \n", *p);
+    p = &b;
+    printf("%d \n", p);
+    printf("%d \n", &a);
+    printf("%d \n", &b);
+    printf("%d \n", &p);
+    printf("%d \n", *p);
     return 0;
 }
